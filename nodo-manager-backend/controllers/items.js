06b06991d0c91f7632ab2909be9c8671cce606d6
MasReed <bluekitdrummer@gmail.com>
2021-05-28 14:59:28 -0500

    implement basic CRUD ops for MenuItems in itemsRouter

--- nodo-manager-backend/controllers/items.js
+++ nodo-manager-backend/controllers/items.js
@@ -1,13 +1,50 @@
 const itemsRouter = require('express').Router()
 const MenuItem = require('../models/menuItem')
 
-// get entire list of menu items
-itemsRouter.get('/', (req, res) => {
-  MenuItem
-    .find({})
-    .then(items =>
-      res.json(items)
-    )
+
+// CREATE new menu item
+itemsRouter.post('/', async (req, res) => {
+  const body = req.body
+
+  const newItem = new MenuItem ({
+    name: body.name,
+    description: body.description,
+    price: body.price,
+    ingredients: body.ingredients,
+    category: body.category,
+  })
+
+  const savedItem = await newItem.save()
+  res.json(savedItem.toJSON())
+})
+
+// READ all menu items
+itemsRouter.get('/', async (req, res) => {
+  const items = await MenuItem.find({})
+  res.json(items)
+})
+
+// UPDATE a menu item
+itemsRouter.put('/:id', async (req, res) => {
+  const body = req.body
+
+  const itemWithUpdates = {
+    name: body.name,
+    description: body.description,
+    price: body.price,
+    ingredients: body.ingredients,
+    category: body.category,
+  }
+
+  const updatedItem = await MenuItem.findByIdAndUpdate(req.params.id, itemWithUpdates, { new: true })
+
+  res.json(updatedItem.toJSON())
+})
+
+// DELETE a menu item
+itemsRouter.delete('/:id', async (req, res) => {
+  await MenuItem.findByIdAndDelete(req.params.id)
+  res.status(204).end()
 })
 
 module.exports = itemsRouter
--- nodo-manager-backend/models/menuItem.js
+++ nodo-manager-backend/models/menuItem.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const menuItemSchema = new mongoose.Schema({
   name: String,
   description: String,
+  price: Number,
   ingredients: Array,
   category: String,
 })
