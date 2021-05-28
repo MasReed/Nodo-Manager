a3f6b8f5e162a39be0b838d7de010a68751e522f
MasReed <bluekitdrummer@gmail.com>
2021-05-28 11:59:25 -0500

    connect to MongoDB Atlas with mongoose, abstract URI key to environment variable

--- nodo-manager-backend/index.js
+++ nodo-manager-backend/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
 const PORT = 3000
@@ -8,12 +9,44 @@ const PORT = 3000
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+
+// DB connection
+mongoose.connect(process.env.MONGODB_URI_NODO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('connected to MongoDB Atlas')
+})
+
+
+
+
+const fakeItem = {
+  name: 'Hamburger',
+  description: 'A plain old hamburger, delicous.',
+  ingredients: ['Bun', 'Patty', 'Lettuce', 'Tomato', 'Pickle', 'Ketchup']
+}
+
+const items = [
+  fakeItem,
+  fakeItem
+]
+
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
 app.get('/api', (req, res) => {
   res.send('API')
+})
+
+app.get('/api/items/', (req, res) => {
+  res.json(items)
 })
 
 
