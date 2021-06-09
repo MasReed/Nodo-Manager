7ec0e539df5022e7c64834ef5f25393424cc9934
MasReed <bluekitdrummer@gmail.com>
2021-06-09 09:15:24 -0500

    create modal window for current order in menupage

--- nodo-manager-frontend/src/components/MenuPage.js
+++ nodo-manager-frontend/src/components/MenuPage.js
@@ -1,25 +1,47 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
-
 import Accordion from 'react-bootstrap/Accordion'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Modal from 'react-bootstrap/Modal'
 
+import { addOrderActionCreator } from '../reducers/orderReducer'
 const MenuPage = () => {
-
+  const dispatch = useDispatch()
   const menuItems = useSelector(state => state.items)
-
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#remove_duplicate_elements_from_the_array
   const categories = useSelector(state => [...new Set(state.items.map(item => item.category))])
+
+  const [showMyOrder, setShowMyOrder] = useState(false)
+  const [orderItems, setOrderItems] = useState([])
 
   const customizeItem = () => {
     console.log('custimze with modal window')
   }
+
+  const addItem = (event) => {
+    event.preventDefault()
+    setOrderItems([...orderItems, 'newItem'])
+  }
+
+  // Order action dispatchers
+  const addOrder = (event) => {
+    event.preventDefault()
+
+    const orderObject = {
+      foodItems: orderItems,
+    }
+
+    dispatch(addOrderActionCreator(orderObject))
+    setShowMyOrder(false)
+    setOrderItems([])
+  }
+
 
   const truncateIngredients = (str) => {
     return str.length > 130 ? str.substring(0, 131) + ' ...' : str
@@ -40,14 +62,16 @@ const MenuPage = () => {
 
 
   return (
-    <Container style={{ padding: '0' }}>
-      <h2>Menu</h2>
+    <Container className='p-0'>
+      <div className='m-0 p-0' style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h2 className='m-0 p-0'>Menu</h2>
+        <Button onClick={() => setShowMyOrder(true)} variant='outline-secondary'>My Order</Button>
+      </div>
       <hr />
 
       {
         categories.map(category => (
           <div key={category}>
-
             <Accordion>
               <Card className='mb-2' style={{ border: 'hidden' }}>
                   <CustomToggle eventKey="0">
@@ -60,7 +84,7 @@ const MenuPage = () => {
                     <Container>
                       <CardDeck>
                         {menuItems.map(item => item.category === category
-                          ? <Col className='container-fluid mb-4 px-2'>
+                          ? <Col key={item._id} className='container-fluid mb-4 px-2'>
                             <Card key={item._id}
                                 className='mx-0 my-0'
                                 style={{
@@ -120,6 +144,42 @@ const MenuPage = () => {
           </div>
           ))
       }
+
+      <Modal
+        show={showMyOrder}
+        onHide={() => {
+          setShowMyOrder(false)
+          setOrderItems([])
+        }}
+        dialogClassName='modal-80w'
+        backdrop="static"
+        keyboard={false}
+        scrollable={true}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Items to Order</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {
+            orderItems.map(item => <p key={item}>{item}</p>)
+          }
+        </Modal.Body>
+
+        <Modal.Footer style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Button
+              variant="outline-warning"
+              onClick={ () => {
+                setShowMyOrder(false)
+                setOrderItems([])
+              }}
+            >Cancel</Button>
+          <div>
+            <Button onClick={ addItem } style={{ margin: '0 10px'}} variant="outline-secondary">Add Item</Button>
+            <Button onClick={ addOrder } style={{ margin: '0 10px'}}>Done</Button>
+          </div>
+        </Modal.Footer>
+      </Modal>
 
     </Container>
   )
