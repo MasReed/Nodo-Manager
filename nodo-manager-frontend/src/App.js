a6d48eead8c5d2209bab8b01200e6e2bff1842ed
MasReed <bluekitdrummer@gmail.com>
2021-06-11 11:15:39 -0500

    convert navbar to react-bootstrap Navbar component, functional links

--- nodo-manager-frontend/src/App.js
+++ nodo-manager-frontend/src/App.js
@@ -7,6 +7,9 @@ import {
   Link
 } from "react-router-dom";
 
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
 import { initializeItems } from './reducers/itemReducer'
 import { initializeOrders } from './reducers/orderReducer'
 import { initializeUsers } from './reducers/userReducer'
@@ -17,6 +20,7 @@ import UsersPage from './components/users/UsersPage'
 import HomePage from './components/HomePage'
 import MenuPage from './components/menu/MenuPage'
 import RegisterPage from './components/RegisterPage'
+import MyOrderPage from './components/myOrder/MyOrderPage'
 
 
 function App() {
@@ -29,25 +33,25 @@ function App() {
       dispatch(initializeUsers())
   }, [ dispatch ])
 
-
-  const navBtnStyle = {
-    padding: '10px',
-    margin: '10px 0'
-  }
-
   return (
     <div>
-      <React.Fragment>
-        <nav style={navBtnStyle}>
-          <Link to='/' style={navBtnStyle}>Home</Link>
-          <Link to='/items' style={navBtnStyle}>Items</Link>
-          <Link to='/menu' style={navBtnStyle}>Menu</Link>
-          <Link to='/orders' style={navBtnStyle}>Orders</Link>
-          <Link to='/users' style={navBtnStyle}>Users</Link>
-          <Link to='/register' style={navBtnStyle}>Register</Link>
-        </nav>
-        <hr />
-      </React.Fragment>
+
+      <Navbar bg="light" expand="lg" sticky='top'>
+        <Navbar.Brand as={Link} to='/'>Nodo-Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to='/menu'>Menu</Nav.Link>
+            <Nav.Link as={Link} to='/my-order'>My Order</Nav.Link>
+            <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+
+          </Nav>
+          <Nav className='ml-auto'>
+            <Nav.Link as={Link} to='/orders'>Orders</Nav.Link>
+            <Nav.Link as={Link} to='/users'>Users</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <React.Fragment>
         <Switch>
@@ -58,6 +62,10 @@ function App() {
 
           <Route path='/menu'>
             <MenuPage />
+          </Route>
+
+          <Route path='/my-order'>
+            <MyOrderPage />
           </Route>
 
           <Route path='/orders'>
