6f2a62d741d88bb5f15a7b11fa310a40a92007ae
MasReed <bluekitdrummer@gmail.com>
2021-06-15 12:25:58 -0500

    Auto scroll pages to top on transitions, sort into site-wide components

---
+++ nodo-manager-frontend/src/components/site-wide/ScrollToTop.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+
+// Credit to Zurfyx:
+// https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition
+
+function ScrollToTop({ history }) {
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0)
+    })
+    return () => {
+      unlisten()
+    }
+  }, [ history ])
+
+  return (null)
+}
+
+export default withRouter(ScrollToTop)
