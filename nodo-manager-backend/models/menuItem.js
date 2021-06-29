f78425d89f14cbf9b8c518d628ea4606d2ebd849
MasReed <bluekitdrummer@gmail.com>
2021-06-29 14:04:49 -0500

    test for creating an item with expected input

--- nodo-manager-backend/models/menuItem.js
+++ nodo-manager-backend/models/menuItem.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const menuItemSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: [true, 'An item name is required.']
+  },
   description: String,
   ingredients: Array,
   category: String,
--- nodo-manager-backend/tests/item_api.test.js
+++ nodo-manager-backend/tests/item_api.test.js
@@ -3,28 +3,79 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const rolesInitializer = require('../utils/rolesInitializer')
+
 const Role = require('../models/role')
+const Item = require('../models/menuItem')
 
 const api = supertest(app)
 
+// Initialize roles database
 beforeAll(() => {
-  return rolesInitializer()
+  rolesInitializer()
+  return
 })
 
-test('roles are initialized', async () => {
-  const roles = await Role.find({}).exec()
-  expect(roles).toHaveLength(5)
+// Reset collections before each test
+beforeEach(async () => {
+  // items
+  await Item.deleteMany({})
 })
 
-test('items are returned as json', async () => {
-  await api
-    .get('/api/items')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+//
+describe('Test Database Initializations', () => {
+  test('roles are initialized', async () => {
+    const roles = await Role.find({}).exec()
+    expect(roles).toHaveLength(5)
+  })
 })
 
+//
+describe('Test Item Type', () => {
+  test('items are returned as json', async () => {
+    await api
+      .get('/api/items')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+})
 
+//
+describe('Item CRUD operations', () => {
+  describe('CREATE', () => {
+    test('Successful creation of expected input', async () => {
+      const newItem = ({
+        name: 'Test Name',
+        description: 'This is a description for this test.',
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+        category: 'Tester',
+        price: 9999.99,
+        availability: 'Available'
+      })
 
+      await api
+        .post('/api/items')
+        .send(newItem)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    // test('Failure upon empty input', async () => {
+    //   const newItem = ({})
+    //
+    //   try {
+    //     await api
+    //       .post('/api/items')
+    //       .send(newItem)
+    //       .expect(400)
+    //       .expect('Content-Type', /application\/json/)
+    //   } catch (err) {
+    //     expect(err).toMatch('ValidationError')
+    //   }
+    // })
+  })
+})
+
+// Close database connection
 afterAll( async () => {
   await mongoose.connection.close()
 })
