eac3d1d6218ea75a908222750c6f7cf0021a601c
MasReed <bluekitdrummer@gmail.com>
2021-07-02 12:00:50 -0500

    refactor authJWT for token and role verification

--- nodo-manager-backend/utils/auth/authJWT.js
+++ nodo-manager-backend/utils/auth/authJWT.js
@@ -17,24 +17,24 @@ const verifyToken = async (req, res, next) => {
     return next()
 
   } catch (err) {
-    console.log('authJWT verify token: ', err)
+    // console.log('authJWT verify token: ', err)
     next(err)
   }
 }
 
 // Check if user has admin authorizations
 const isAdmin = async (req, res, next) => {
-
   try {
-    const user = await User.findById(req.userId)
-    const userRoles = await Role.find({ _id: { $in: user.roles } })
+    const user = await User
+      .findById(req.userId)
+      .populate('role', '-__v')
+      .exec()
 
-    for (let i = 0; i < userRoles.length; i++) {
-      if (userRoles[i].name === 'admin') {
-        return next()
-      }
+    if (user.role.encompassedRoles.includes('admin')) {
+      return next()
     }
-    throw ({ status: 403, message: 'Require Admin Role' })
+
+    throw ({ status: 403, message: 'Requires Admin Role' })
 
   } catch (err) {
     next(err)
@@ -43,17 +43,56 @@ const isAdmin = async (req, res, next) => {
 
 // Check if user has manager authorizations
 const isManager = async (req, res, next) => {
-
   try {
-    const user = await User.findById(req.userId)
-    const userRoles = await Role.find({ _id: { $in: user.roles } })
+    const user = await User
+      .findById(req.userId)
+      .populate('role', '-__v')
+      .exec()
 
-    for (let i = 0; i < roles.length; i++) {
-      if (userRoles[i].name === 'manager') {
-        return next()
-      }
+    if (user.role.encompassedRoles.includes('manager')) {
+      return next()
     }
-    throw ({ status: 403, message: 'Require Manager Role' })
+
+    throw ({ status: 403, message: 'Requires Manager Role' })
+
+  } catch (err) {
+    next(err)
+  }
+}
+
+
+// Check if user has employee authorizations
+const isEmployee = async (req, res, next) => {
+  try {
+    const user = await User
+      .findById(req.userId)
+      .populate('role', '-__v')
+      .exec()
+
+    if (user.role.encompassedRoles.includes('employee')) {
+      return next()
+    }
+
+    throw ({ status: 403, message: 'Requires Employee Role' })
+
+  } catch (err) {
+    next(err)
+  }
+}
+
+// Check if user has user authorizations
+const isUser = async (req, res, next) => {
+  try {
+    const user = await User
+      .findById(req.userId)
+      .populate('role', '-__v')
+      .exec()
+
+    if (user.role.encompassedRoles.includes('user')) {
+      return next()
+    }
+
+    throw ({ status: 403, message: 'Requires User Role' })
 
   } catch (err) {
     next(err)
@@ -63,7 +102,9 @@ const isManager = async (req, res, next) => {
 const authJwt = {
   verifyToken,
   isAdmin,
-  isManager
+  isManager,
+  isEmployee,
+  isUser
 }
 
 module.exports = authJwt
