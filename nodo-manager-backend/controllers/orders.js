fa694283c90fd91259f2986ec3bccd7ab3f9cf2c
MasReed <bluekitdrummer@gmail.com>
2021-07-08 10:55:39 -0500

    order controller now sends errors to error handler, update todos

--- nodo-manager-backend/controllers/orders.js
+++ nodo-manager-backend/controllers/orders.js
@@ -1,65 +1,90 @@
 const ordersRouter = require('express').Router()
 const Order = require('../models/order')
 const authJwt = require('../utils/auth/authJWT')
-const verifyOrder = require('../utils/verifyOrder')
+const orderValidation = require('../utils/validations/orderValidation')
 
 
 // CREATE new order
-ordersRouter.post('/', verifyOrder.verifyCosts, async (req, res) => {
-  const body = req.body
+ordersRouter.post('/', orderValidation.verifyCosts, async (req, res, next) => {
+  try {
+    const body = req.body
 
-  const newOrderObject = new Order({
-    time: Date.now(),
-    status: body.status,
-    category: body.category,
-    name: body.name,
-    items: body.items,
-    notes: body.notes,
-    subTotal: body.subTotal,
-    taxRate: body.taxRate,
-    taxAmount: body.taxAmount,
-    total: body.total
-  })
+    const newOrderObject = new Order({
+      time: Date.now(),
+      status: body.status,
+      category: body.category,
+      name: body.name,
+      items: body.items,
+      notes: body.notes,
+      subTotal: body.subTotal,
+      taxRate: body.taxRate,
+      taxAmount: body.taxAmount,
+      total: body.total
+    })
 
-  const savedOrder = await newOrderObject.save()
-  res.json(savedOrder.toJSON())
+    const savedOrder = await newOrderObject.save()
+    res.json(savedOrder.toJSON())
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 // READ all orders
-ordersRouter.get('/', authJwt.verifyToken, async (req, res) => {
-  const orders = await Order.find({})
-  res.json(orders)
+ordersRouter.get('/', authJwt.verifyToken, async (req, res, next) => {
+  try {
+    const orders = await Order.find({})
+    res.json(orders)
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 // READ an order
-ordersRouter.get('/:id', async (req, res) => {
-  const order = await Order.findById(req.params.id)
-  res.json(order)
+ordersRouter.get('/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id)
+    res.json(order)
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 // UPDATE an order
-ordersRouter.put('/:id', verifyOrder.verifyCosts, async (req, res) => {
-  const body = req.body
-  const orderWithUpdates = {
-    time: Date.now(),
-    status: body.status,
-    category: body.category,
-    name: body.name,
-    items: body.items,
-    notes: body.notes,
-    subTotal: body.subTotal,
-    taxRate: body.taxRate,
-    taxAmount: body.taxAmount,
-    total: body.total
+ordersRouter.put('/:id', orderValidation.verifyCosts, async (req, res, next) => {
+  try {
+    const body = req.body
+    const orderWithUpdates = {
+      time: Date.now(),
+      status: body.status,
+      category: body.category,
+      name: body.name,
+      items: body.items,
+      notes: body.notes,
+      subTotal: body.subTotal,
+      taxRate: body.taxRate,
+      taxAmount: body.taxAmount,
+      total: body.total
+    }
+    const updatedOrder = await Order.findByIdAndUpdate(req.params.id, orderWithUpdates, { new: true })
+    res.json(updatedOrder.toJSON())
+    
+  } catch (err) {
+    next(err)
   }
-  const updatedOrder = await Order.findByIdAndUpdate(req.params.id, orderWithUpdates, { new: true })
-  res.json(updatedOrder.toJSON())
 })
 
 // DELETE an order
-ordersRouter.delete('/:id', async (req, res) => {
-  const deletedOrder = await Order.findByIdAndDelete(req.params.id)
-  res.json(deletedOrder.toJSON())
+ordersRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const deletedOrder = await Order.findByIdAndDelete(req.params.id)
+    res.json(deletedOrder.toJSON())
+
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = ordersRouter
