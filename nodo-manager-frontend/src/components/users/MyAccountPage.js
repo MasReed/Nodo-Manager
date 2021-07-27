295d24ca31224a87a204e0bfa62bfdc1a4e3f695
MasReed <bluekitdrummer@gmail.com>
2021-07-27 16:30:12 -0500

    minor updates to MyAccountPage

--- nodo-manager-frontend/src/components/users/MyAccountPage.js
+++ nodo-manager-frontend/src/components/users/MyAccountPage.js
@@ -8,19 +8,16 @@ const MyAccountPage = () => {
 
   return (
     <Container className='pt-5'>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div className='d-flex justify-content-between'>
         <h1 className='m-0'>My Account</h1>
         <h1>{currentUser.name}</h1>
       </div>
       <hr />
-      <p>
-        Username:
-        {currentUser.username}
-      </p>
-      <p>
-        Email:
-        {currentUser.email}
-      </p>
+      <p>{`Name: ${currentUser.name}`}</p>
+      <p>{`Email: ${currentUser.email}`}</p>
+      <p>{`Username: ${currentUser.username}`}</p>
+      <p>{`Role: ${currentUser.role.name}`}</p>
+
     </Container>
   )
 }
