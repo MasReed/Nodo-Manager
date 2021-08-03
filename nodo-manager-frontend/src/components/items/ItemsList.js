75ab09054878966ce15911e90c18bf5910476aca
MasReed <bluekitdrummer@gmail.com>
2021-08-03 12:30:28 -0500

    improve item card responsiveness

--- nodo-manager-frontend/src/components/items/ItemsList.js
+++ nodo-manager-frontend/src/components/items/ItemsList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import CardDeck from 'react-bootstrap/CardDeck'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 import ItemInfoCard from './ItemInfoCard'
 
@@ -12,18 +12,17 @@ const ItemList = ({ category }) => {
 
   return (
     <Container>
-      <CardDeck>
-
+      <Row xs={1} md={2} lg={2}>
         {/* Items with matching 'category' are rendered */}
         {items.map((item) => (item.category === category
           ? (
-            <Col key={item._id} className='container-fluid mb-4 px-2'>
+            <Col className='mx-0 mb-4'>
               <ItemInfoCard key={item._id} item={item} />
             </Col>
+
           )
           : null))}
-
-      </CardDeck>
+      </Row>
 
       <hr />
 
