5ecbf8609c40bfffa079d2b42f47beea5d2766c9
MasReed <bluekitdrummer@gmail.com>
2021-08-12 13:46:24 -0500

    update server to pull frontend from nested folder, attempt to set babel-jest version in package.json

--- nodo-manager-backend/app.js
+++ nodo-manager-backend/app.js
@@ -42,9 +42,9 @@ app.use('/api/authenticate', authenticationsRouter)
 
 // Serve React App in production environment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/../nodo-manager-frontend', 'build')))
+  app.use(express.static(path.join(__dirname, 'nodo-manager-frontend', 'build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../nodo-manager-frontend', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'nodo-manager-frontend', 'build', 'index.html'))
   })
 }
 
