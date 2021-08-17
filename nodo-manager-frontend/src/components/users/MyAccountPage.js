a2e7494a81f5a930e4ce11b110957ffd60dd1eb1
MasReed <bluekitdrummer@gmail.com>
2021-08-17 11:11:26 -0500

    improve my account layout, currently shows previous orders

--- nodo-manager-frontend/src/components/users/MyAccountPage.js
+++ nodo-manager-frontend/src/components/users/MyAccountPage.js
@@ -9,16 +9,30 @@ const MyAccountPage = () => {
   return (
     <Container className='pt-5'>
       <div className='d-flex justify-content-between'>
-        <h1 className='m-0'>My Account</h1>
-        <h1>{currentUser.name}</h1>
+        <div className='m-0'>
+          <h1 className='m-0'>My Account</h1>
+          {/* Only show role for employees and above */}
+          {
+            currentUser.role.encompassedRoles.includes('employee')
+              ? (
+                <h6 className='my-2'>
+                  <div className='d-inline pr-1'>{currentUser.role.name}</div>
+                  <p className='d-inline'>|</p>
+                  <div className='d-inline pl-1'>{currentUser.email}</div>
+                </h6>
+              )
+              : (
+                <h6 className='m-0 mt-2'>{currentUser.email}</h6>
+              )
+          }
+        </div>
+        <div className='m-0'>
+          <h1 className='m-0'>{currentUser.name}</h1>
+          <h6 className='m-0 mt-2 text-right'>{currentUser.username}</h6>
+        </div>
       </div>
 
-      <hr />
-
-      <p>{`Name: ${currentUser.name}`}</p>
-      <p>{`Email: ${currentUser.email}`}</p>
-      <p>{`Username: ${currentUser.username}`}</p>
-      <p>{`Role: ${currentUser.role.name}`}</p>
+      <hr className='mt-2' />
 
       <h2 className='pt-5'>Previous Orders</h2>
 
